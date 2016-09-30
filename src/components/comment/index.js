7e8a955b426c8f13b6a9d16b48b9d434df5a5b3a
Matt Pivnick <mpivnick@ebsco.com>
2016-09-30 14:40:18 -0400

    fix warning, do not track the bundle built in webpack

--- src/components/comment/index.js
+++ src/components/comment/index.js
@@ -4,10 +4,8 @@ import ReactDOM from 'react-dom';
 const Comment = ({
 	text,
 	author,
-	key,
 	...props
 }) => {
-	console.log('keeey', key, this);
 	return (
 		<div className="comment">
 			<h2 className="commentAuthor">
