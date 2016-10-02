b447dc61d2d670377506044061e1155c1366a12d
Matt Pivnick <mpivnick@ebsco.com>
2016-10-01 21:44:31 -0400

    adding a test component to play with react-router

--- src/client.js
+++ src/client.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './components/commentBox'
+import Test from './components/test'
+import { Router, Route, Link, hashHistory } from 'react-router'
 
-ReactDOM.render(
-  <CommentBox url="/api/comments" pollInterval={2000} />,
-  document.getElementById('root')
+const App = ({
+	...props
+}) => {
+	return (
+		<div>
+			<div><Link to="/test">About</Link></div>
+			<CommentBox url="/api/comments" pollInterval={2000} />
+		</div>
+	)
+}
+
+ReactDOM.render((
+		<Router history={hashHistory}>
+			<Route path="/" component={App} />
+			<Route path="/test" component={Test} />
+		</Router>
+	),
+	document.getElementById('root')
 );
---
+++ src/components/test/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Test = ({
+	...props
+}) => {
+	return (
+		<div>
+			HELLO NEW PAGE!
+		</div>
+	);
+};
+
+export default Test;
